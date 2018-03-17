fd2739a5e80612131984c61fd6d8efad8b88c798
bozis <busraozis1994@hotmail.com>
2018-03-17 23:17:34 +0300

    contact list opens

--- app/src/main/java/com/example/bra/emergentcall/EmergentCall.java
+++ app/src/main/java/com/example/bra/emergentcall/EmergentCall.java
@@ -3,32 +3,37 @@ package com.example.bra.emergentcall;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
-/*import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;*/
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.appindexing.Thing;
+
+/*import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing; */
 
 public class EmergentCall extends AppCompatActivity {
     private FusedLocationProviderClient mFusedLocationClient;
@@ -37,11 +42,17 @@ public class EmergentCall extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    private final int PICK_CONTACT = 1; //birden çok temas adresi seçimi için listeleştirilebilir.
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_emergent_call);
+        setContentView(R.layout.activity_emergent_call);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
@@ -88,28 +99,31 @@ public class EmergentCall extends AppCompatActivity {
         List<String> phoneNumbers = new ArrayList<>();
         phoneNumbers.add(0, "+905339677890");
 
-        String smsBody = "Ben Büşra. Acil durum mesajıdır. Yardımına ihtiyacım var. Konum bilgim şöyle: http://maps.google.com/?q="+myCurrentLocation.getLatitude()+","+myCurrentLocation.getLongitude();
+        String smsBody = "Ben Büşra. Acil durum mesajıdır. Yardımına ihtiyacım var. Konum bilgim şöyle: http://maps.google.com/?q=" + myCurrentLocation.getLatitude() + "," + myCurrentLocation.getLongitude();
 
 
         Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:"+phoneNumbers.get(0)));
+        callIntent.setData(Uri.parse("tel:" + phoneNumbers.get(0)));
 
 
         //sms otomatik atılacak, benim göndermemi beklemeyecek. Konum bilgisi gelmiyor. Başarılı bir şekilde alınması sağlanacak.
         Intent smsIntent = new Intent(Intent.ACTION_SENDTO,
-                Uri.parse("sms:"+phoneNumbers.get(0)));
+                Uri.parse("sms:" + phoneNumbers.get(0)));
         smsIntent.putExtra("sms_body", smsBody);
-        startActivity(smsIntent);
+        //startActivity(smsIntent);
 
         if (ActivityCompat.checkSelfPermission(EmergentCall.this,
                 Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         System.out.println("It will call " + phoneNumbers.get(0));
-        startActivity(callIntent);
+        //startActivity(callIntent);
         System.out.println("It must have called!");
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+      //  client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -134,5 +148,78 @@ public class EmergentCall extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void callContacts(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
 
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(new File("contacts"),true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ;
+
+        if (reqCode == PICK_CONTACT) {
+            if (resultCode == ActionBarActivity.RESULT_OK) {
+                Uri contactData = data.getData();
+                Cursor c = getContentResolver().query(contactData,null,null,null,null);
+
+
+                while(c.moveToNext()){
+                    String name = c.getString(c.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String phone = c.getString(c.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    phone += phone+"\n";
+                    Toast.makeText(this, "Seçtiğiniz kişi: "+ name, Toast.LENGTH_LONG).show();
+                    try {
+                        outputStream.write(phone.getBytes());
+                        outputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    /*public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("EmergentCall Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2.connect();
+        AppIndex.AppIndexApi.start(client2, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client2, getIndexApiAction());
+        client2.disconnect();
+    }*/
 }
