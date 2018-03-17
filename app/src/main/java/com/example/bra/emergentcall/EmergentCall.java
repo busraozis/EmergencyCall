d948b87bae23948cd6c86c0d454e5a90c550c32f
bozis <busraozis1994@hotmail.com>
2018-03-17 23:21:21 +0300

    notify function constructed

--- app/src/main/java/com/example/bra/emergentcall/EmergentCall.java
+++ app/src/main/java/com/example/bra/emergentcall/EmergentCall.java
@@ -56,6 +56,84 @@ public class EmergentCall extends AppCompatActivity {
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
+        /*mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        final Location myCurrentLocation = new Location("dummy");
+        myCurrentLocation.setLongitude(0.0);
+        myCurrentLocation.setLatitude(0.0);
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+
+                            /*String msg = " Current Location: " +
+                                    Double.toString(location.getLatitude()) + ",    " +
+                                    Double.toString(location.getLongitude());
+                            System.out.println(msg);
+                            ((TextView)findViewById(R.id.content_emergent_call)).setText(msg);*/
+
+                            //Set my current location
+                            myCurrentLocation.setLatitude(location.getLatitude());
+                            myCurrentLocation.setLongitude(location.getLongitude());
+                    //    }
+                  //  }
+                //});
+
+
+        //SmsManager.getDefault().sendTextMessage(phoneNumbers.get(0), null, smsBody, null,null);
+        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumbers.get(0)));
+        //intent.putExtra("sms_body", smsBody);
+        //startActivity(intent);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        /*List<String> phoneNumbers = new ArrayList<>();
+        phoneNumbers.add(0, "+905339677890");
+
+        String smsBody = "Ben Büşra. Acil durum mesajıdır. Yardımına ihtiyacım var. Konum bilgim şöyle: http://maps.google.com/?q=" + myCurrentLocation.getLatitude() + "," + myCurrentLocation.getLongitude();
+
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + phoneNumbers.get(0)));
+
+
+        //sms otomatik atılacak, benim göndermemi beklemeyecek. Konum bilgisi gelmiyor. Başarılı bir şekilde alınması sağlanacak.
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO,
+                Uri.parse("sms:" + phoneNumbers.get(0)));
+        smsIntent.putExtra("sms_body", smsBody);
+        //startActivity(smsIntent);
+
+        if (ActivityCompat.checkSelfPermission(EmergentCall.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        System.out.println("It will call " + phoneNumbers.get(0));
+        //startActivity(callIntent);
+        System.out.println("It must have called!");*/
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+      //  client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_emergent_call, menu);
+        return true;
+    }
+
+    public void notify(){
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         final Location myCurrentLocation = new Location("dummy");
@@ -123,14 +201,7 @@ public class EmergentCall extends AppCompatActivity {
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-      //  client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_emergent_call, menu);
-        return true;
+        //  client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
